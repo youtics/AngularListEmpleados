22dd1168ad3eb55f073e57eb27db60de57a33ee7
gabriel chaldu <youtics@gmail.com>
2021-08-23 11:31:05 -0300

    Version con eliminar por legajo

--- src/app/components/empleado-crear/empleado-crear.component.ts
+++ src/app/components/empleado-crear/empleado-crear.component.ts
@@ -1,5 +1,5 @@
-import { EmpleadoListComponent } from './../empleado-list/empleado-list.component';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Empleado } from 'src/app/models/Empleado';
 
 @Component({
@@ -9,15 +9,50 @@ import { Empleado } from 'src/app/models/Empleado';
 })
 export class EmpleadoCrearComponent implements OnInit {
 
-  lista?: EmpleadoListComponent;
-  constructor() { }
+  @Input() lista:Empleado[] = [];
+  legajoEliminar:string='-1';
+  form:FormGroup;
+  
+  constructor(private formBuilder:FormBuilder) { 
+    this.form = this.formBuilder.group({
+      legajo:['', [Validators.required]],
+      apellido:['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {
   }
 
-  agregarEmpleadoalArray()
+  VerArreglo()
   {
-    var e:Empleado = new Empleado(6,'Pepe', 'Lauau',2150,'Masculino');
-    this.lista?.listaDeEmpleados.push(e);
+    
+  }
+  buscarElemento(aBuscarLegajo: any):number
+  {
+    var i=0;
+    var flag=0;
+    var pos=-1;
+    for(i=0;  i<this.lista.length  && flag==0 ;i++)
+    {
+      if(this.lista[i].Legajo.toString() === aBuscarLegajo)
+      {
+        flag=1;
+        pos=i;
+      }
+    }
+    return pos;
+  }
+  eliminarElemento(desde:number)
+  {
+    this.lista.splice(desde,1);
+  }
+
+  controladorEliminar()
+  {
+    this.legajoEliminar = this.form.get('legajo')?.value;
+    console.log(this.legajoEliminar);
+    var posicion = this.buscarElemento(this.form.get('legajo')?.value);
+    if(posicion!=-1)
+      this.eliminarElemento(posicion);
   }
 }
